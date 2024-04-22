b329cbbae1c122c4d3539a0d4b051b37f473951c
AlinaCosta <alinacosta87@gmail.com>
2024-04-22 21:35:02 +0300

    add navigation

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import { HiOutlineMenu, HiOutlineX } from "react-icons/hi";
 
-// import styles from "./Navbar.module.css";
+import styles from "./Navbar.module.css";
+import images from "../../utils/images";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.navbar__logo}>
+        <img src={images.logo2} alt="logo" />
+      </div>
+      <ul className={styles.navbar__links}>
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#projects">Projects</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <div className={styles.mobileMenu}>
+        <HiOutlineMenu fontSize={30} onClick={() => setToggleMenu(true)} />
+        {/* i am showing this div only if the toggleMenu in set to true */}
+        {toggleMenu && (
+          <div
+            className={`${styles.mobileMenu__overlay} flex__center slide-bottom`}
+          >
+            <HiOutlineX
+              className={styles.overlay__close}
+              onClick={() => setToggleMenu(false)}
+            />
+            <ul className={styles.mobileMenu__links}>
+              <li>
+                <a href="#home">Home</a>
+              </li>
+              <li>
+                <a href="#about">About</a>
+              </li>
+              <li>
+                <a href="#projects">Projects</a>
+              </li>
+              <li>
+                <a href="#services">Services</a>
+              </li>
+              <li>
+                <a href="#contact">Contact</a>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
---
+++ src/utils/images.js
@@ -0,0 +1,5 @@
+/* eslint-disable import/no-anonymous-default-export */
+import logo from "../assets/alinacosta-logo.png";
+import logo2 from "../assets/alinacosta-logo-2.png";
+
+export default { logo, logo2 };
